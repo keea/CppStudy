43cc60c16e0f210056fce58be437d4b3b3d71d5f
keea <heea44@naver.com>
2017-07-13 11:50:43 +0900

    NameSpace

---
+++ Problem01_4/Problem01_4/func.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace BestComImp1 {
+	void SimpleFunc(void);
+}
+
+namespace ProgComImp1 {
+	void SimpleFunc(void);
+}
---
+++ Problem01_4/Problem01_4/main.cpp
@@ -0,0 +1,16 @@
+/*
+* 
+* 파일의 분할
+*
+* @date     Thu Jul 13 11:21:02 2017
+* @author   keea
+*/
+
+#include "func.h";
+
+int main(void) {
+	BestComImp1::SimpleFunc();
+	ProgComImp1::SimpleFunc();
+
+	return 0;
+}
