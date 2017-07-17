85492e21b37b3109afa322cb9a72d8df0baf3bec
keea <heea44@naver.com>
2017-07-17 14:26:32 +0900

    struct

---
+++ Problem03_1/Problem03_1/source.cpp
@@ -0,0 +1,40 @@
+/*
+*
+* ÁÂÇ¥¸¦ Ç¥Çö
+*
+* @date     Mon Jul 17 14:18:03 2017
+* @author   keea
+*/
+
+#include <iostream>
+
+struct Point
+{
+	int xpos;
+	int ypos;
+
+	void MovePos(int x, int y) {
+		xpos += x;
+		ypos += y;
+	}
+
+	void AddPoint(const Point &pos) {
+		xpos += pos.xpos;
+		ypos += pos.ypos;
+	}
+
+	void ShowPostition() {
+		std::cout << "[ " << xpos << " , " << ypos << " ] " << std::endl;
+	}
+};
+
+void main() {
+	Point pos1 = { 12,4 };
+	Point pos2 = { 20,30 };
+
+	pos1.MovePos(-7, 10);
+	pos1.ShowPostition();
+
+	pos1.AddPoint(pos2);
+	pos1.ShowPostition();
+}
